2b461623092aa60a183597d584fcf4e7b47e3f73
GiftSibiya <sibiya.bobo@gmail.com>
2023-12-20 15:48:41 +0200

    Add project files.

---
+++ Models/Transaction.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        [Column(TypeName = "nvarchar(12)")]
+        public String AccountNumber { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+
+        public int BeneficiaryName { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+
+        public int BankName { get; set; }
+        [Column(TypeName = "nvarchar(11)")]
+
+        public int SwiftCode { get; set; }
+        public int Amount { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
